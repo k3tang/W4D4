361e78ff277f469a93e81302031a6ec2eaee49fa
Karen Siu <k3tang@gmail.com>
2022-07-07 16:44:13 -0700

    add moves and game logic

--- Hanoi/board.rb
+++ Hanoi/board.rb
@@ -1,18 +1,55 @@
 require_relative 'player.rb'
 
 class Board
+    attr_reader :stacks, :player
     def initialize
-        @board = Array.new(3){[]}
+        @stacks = Array.new(3){[]}
+        @player = Player.new
+        
     end
-
 
     def populate(level)
-        level.downto(0).each do |num|
-            @board[0] << [num]
+        level.downto(1).each do |num|
+            stacks[0] << [num]
         end
-        @board
+        stacks
     end
+
+    def display
+       stacks.each {|subarr| p subarr}
+    end
+
+    def move_disk
+        pos = player.get_move
+        init_stack = pos[0] - 1
+            raise error "not valid inital stack position" if init_stack > 3
+            raise error "empty stack, try again" if stacks[init_stack].empty?
+        pop_disk = stacks[init_stack].pop 
+        end_stack = pos[1] - 1
+            unless (stacks[end_stack].empty? || pop_disk > stacks[end_stack][-1]) 
+                raise error "this disk is too big"
+            end 
+        stacks[end_stack] << pop_disk
+            return true 
+    end 
+
+    def win?
+        newarr = []
+        (1..8).reverse_each do |i|
+            newarr << [i]
+        end 
+
+        if stacks[2] == newarr
+            p "You won!"
+            return true
+        else
+            return false
+        end
+    end 
+
+  
 end
 
 b = Board.new
-p b.populate(8)
+b.populate(8)
+p b.move_disk
--- Hanoi/player.rb
+++ Hanoi/player.rb
@@ -1,13 +1,18 @@
 class Player
     def initialize
-        @player = player
+    
     end
 
     def get_move
-        puts "please enter the stack number you want to move"
+            newarr = []
+        while newarr.length < 2 
+        puts "please enter the stack number (1-3) you want to move"
         init_stack = gets.chomp.to_i
+            newarr << init_stack
         puts "enter its destination stack"
         end_stack = gets.chomp.to_i
-        return [init_stack,end_stack]
+            newarr << end_stack
+        end 
+       return newarr
     end
 end
